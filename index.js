b3203d9fcf85d20d4320f7b13a856f1c1dc39c07
emmerz6949 <emmerz6949@gmail.com>
2020-08-14 20:24:07 -0400

    added a start function

--- index.js
+++ index.js
@@ -17,5 +17,53 @@ const connection = mysql.createConnection({
 
 connection.connect(function(err) {
   if (err) throw err;
-
+  start();
 });
+
+async function start() {
+    try {
+        const whatDo = await inquirer.prompt({
+            name: "yado",
+            type: "list",
+            message: "What would you like to do?",
+            choices: [
+              "View All Departments",
+              "View All Roles",
+              "View All Employees",
+              "Add a Department",
+              "Add a Role",
+              "Add an Employee",
+              "Update an Existing Employee's Role",
+              "Exit"
+            ]
+        });
+        const {yado} = whatDo;
+        if (yado === "View All Departments") {
+            viewD();
+        }
+        else if (yado === "View All Roles") {
+            viewR();
+        }
+        else if (yado === "View All Employees") {
+            viewE();
+        }
+        else if (yado === "Add a Department") {
+            addD();
+        }
+        else if (yado === "Add a Role") {
+            addR();
+        }
+        else if (yado === "Add an Employee") {
+            addE();
+        }
+        else if (yado === "Update an Existing Employee's Role") {
+            updateER();
+        }
+        else {
+            connection.end();
+        }
+
+    } catch (err) {
+        console.log(err);
+    }
+}
